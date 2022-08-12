a55cf2a8e55468f8a2178c15fe01d64bdaebde3c
Zurisha <andreamwest7@gmail.com>
2022-08-12 10:14:05 -0500

    added user model and resolvers"

--- server/server.js
+++ server/server.js
@@ -18,7 +18,6 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
