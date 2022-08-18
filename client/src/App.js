0423357c5cdffdf714df9d972d47dec99dff07cb
Zurisha <andreamwest7@gmail.com>
2022-08-17 23:16:19 -0500

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+
+} from '@apollo/client'; 
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import Login from './pages/Login';
+import Eat from './components/Eat';
+import Sleep from './components/Sleep';
+import Workout from './components/Workout';
+import Signup from './pages/Signup';
+import NoMatch from './pages/NoMatch';
+import Contact from './components/Contact';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return{
+    headers: { 
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <ApolloProvider client={client}>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Nav />
+      <div className='container'>
+        <Routes>
+          <Route
+            path='/'
+            element={<Home />}
+            />
+          <Route
+            path='/login'
+            element={<Login/>}
+            />
+          
+          <Route
+            path='/eat'
+            element={<Eat/>}
+            />
+          <Route
+            path='/sleep'
+            element={<Sleep/>}
+            />
+          <Route
+            path='/workout'
+            element={<Workout/>}
+            />
+          <Route
+            path='/Contact'
+            element={<Contact/>}
+            />
+          <Route
+            path='/Signup'
+            element={<Signup/>}
+            />
+          <Route 
+            path='*'
+            element={<NoMatch />}
+            />
+        </Routes>
+      </div>
+      <Footer />
+      </div>
+    </BrowserRouter>
+   </ApolloProvider>
   );
 }
 
---
+++ client/src/components/Sleep/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sleep = () => {
+return (
+    <div>
+        Sleep
+    </div>
+)
+}
+
+export default Sleep;
